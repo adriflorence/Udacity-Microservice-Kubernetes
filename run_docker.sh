2d0e92a929f4c20862da59f67c840de727a6624f
Adri Forczek <forczekadri@gmail.com>
2020-02-15 23:44:52 +0000

    chore: completes run_docker shell script

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+## steps that allow run Docker locally
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=localdocker .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it localdocker bash
